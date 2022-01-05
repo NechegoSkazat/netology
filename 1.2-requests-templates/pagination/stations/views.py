f5030e8c97c6f440edcb2fac2d7cd3cb36d0d6b2
Дмитрий <kotleta88@gmail.com>
2022-01-05 19:08:06 +0300

    Вынес чтение данных из csv за пределы view-функции. Иисправил передачу данных о станциях в контекст.

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,11 +1,19 @@
-from pprint import pprint
-
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 import csv
 
 from pagination import settings
+
+bus_stations_list = []
+with open(settings.BUS_STATION_CSV, 'r', encoding='utf-8') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        name = row['Name']
+        street = row['Street']
+        district = row['District']
+        bus_station = {'Name': name, 'Street': street, 'District': district}
+        bus_stations_list.append(bus_station)
 
 
 def index(request):
@@ -15,19 +23,10 @@ def index(request):
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-    bus_stations = []
-    with open(settings.BUS_STATION_CSV, 'r', encoding='utf-8') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            name = row['Name']
-            street = row['Street']
-            district = row['District']
-            bus_station = {'Name': name, 'Street': street, 'District': district}
-            bus_stations.append(bus_station)
     page_number = int(request.GET.get("page", 1))
-    paginator = Paginator(bus_stations, 10)
+    paginator = Paginator(bus_stations_list, 10)
     page = paginator.get_page(page_number)
-    bust_stations_page = paginator.page(page_number)
+    bust_stations_page = page.object_list
     context = {
         'bus_stations': bust_stations_page,
         'page': page
