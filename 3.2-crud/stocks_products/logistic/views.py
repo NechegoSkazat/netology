17727d16d54a27d8b9f19472961ae166481ee790
Дмитрий <kotleta88@gmail.com>
2022-02-27 23:57:41 +0300

    Выполнил ДЗ по CRUD

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,16 +1,23 @@
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock
+from \
+    .models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['title', 'description']
+    filterset_fields = ['id']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    filterset_fields = ['products']
+    search_fields = ['positions__product__id']
